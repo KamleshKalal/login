1f04508348f4690f267d876affdf68a0f2df445c
KamleshKalal <kamleshkalal501@gmail.com>
2023-04-02 21:47:27 +0530

    com

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+require("./db/conn");
+const port = process.env.PORT || 3000;
+
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partial_path = path.join(__dirname, "../templates/partials");
+app.use(express.static(static_path));
+
+app.set("view engine", "hbs");
+app.set("views", template_path);
+hbs.registerPartials(partial_path);
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+
+app.listen(port, () => {
+  console.log("Listening on port", port);
+});
